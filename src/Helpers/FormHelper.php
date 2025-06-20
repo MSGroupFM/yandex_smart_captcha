78092513fa7bd7bfdbe0f20a27527bb5de2a58f6
MSGroupFM <admin@msgru.com>
2025-06-21 02:50:59 +0300

    Fix license in files

--- src/Helpers/FormHelper.php
+++ src/Helpers/FormHelper.php
@@ -3,7 +3,7 @@
  * @package     yandex_smart_captcha
  * @author      Vladislav Tsygankov
  * @copyright   Copyright (c) 2025 MSGru. All rights reserved.
- * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @license     MIT
  * @link        https://msgru.com/
  */
 
