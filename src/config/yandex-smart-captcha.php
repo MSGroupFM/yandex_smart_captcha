75e6a02098dc4c2f94e06f2f5f3f702c6f41b666
MSGroupFM <admin@msgru.com>
2025-06-21 03:02:06 +0300

    Fix config

--- src/config/yandex-smart-captcha.php
+++ src/config/yandex-smart-captcha.php
@@ -1,4 +1,5 @@
+<?php
 return [
-'sitekey' => env('YANDEX_SMARTCAPTCHA_SITEKEY'),
-'secret' => env('YANDEX_SMARTCAPTCHA_SECRET'),
+    'sitekey' => env('YANDEX_SMARTCAPTCHA_SITEKEY'),
+    'secret'  => env('YANDEX_SMARTCAPTCHA_SECRET'),
 ];
